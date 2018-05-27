00fdc1ff0301ee1a5a11e9eb6cb31c5fcd98eec9
Daniel McGrath <demcgrath2@gmail.com>
2018-05-27 15:38:09 -0700

    Update useful_bash.sh

--- bash/useful_bash.sh
+++ bash/useful_bash.sh
@@ -1,65 +1,47 @@
-# Use these lines in terminal on your mac
-# 1) username:~ "line"
-# 2) Hit Enter
+#------------------
+# SOME USEFUL BASH
+#------------------
 
-#nano script
-    #from command line:
-    >> nano script.sh
-
-    #in the nano file:
-
-        #!/bin/bash
-        echo This is a Bash script # simple example that prints this message when script is run
-
-    #from command line:
-    username:~ chmod +x script.sh
-    username:~ ./script.sh # run the script
-
-#declare an array
-declare -a a
-
-#declare an integer
+# declare an integer
 declare -i index=0
 
-#wait n seconds
-sleep n
+# declare a boolean named bool equal to true
+bool = true
 
-#find out what directory you're in
-pwd
+# declare an array
+declare -a a
 
-# list all networks your device has been on
-# note this ignores case (-i) and will accept substring arguments (i.e. if you look for "stanford" you need only search for "nfor")
-networksetup -listpreferredwirelessnetworks en0 | grep -i "someString"
-
-# array length
+# get array length
 ${#ArrayName[@]}
 
-#ternary
-[[ $numberOfUniqueWords > 1 ]] && word1="words" || word1="word"
+# get array length of array named 'a'
+declare -i length=${#a[@]}
 
-#string params
-echo "There $word2 $numberOfUniqueWords unique $word1 in this file"
+# place variables within string (variables in this case are called word2, numberOfUniqueWords, and word1)
+"There $word2 $numberOfUniqueWords unique $word1 in this file"
 
-#loop through text file line by line
+# pause for n seconds
+sleep n
+
+# print "Hello, World!" to stdout
+echo Hello, World!
+
+# loop through a text file line by line
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo $line
 done < "$1"
 
-#array length of array 'a'
-declare -i length=${#a[@]}
-
-
-#for loop
+# for loop
 for (( i=0; i<${length}; i++ ));
 do
     echo hi
 done
 
-#boolean named bool equal to true
-bool=true
-
-#if 
+# if statement
 if [ "${a[i]}" == "${a[j]}" ]
 then
     echo they are equal
 fi
+
+# ternary operator of sorts
+[[ $numberOfUniqueWords > 1 ]] && word1="words" || word1="word"
